dc667c92f98300e83bbb9c8f364e18f516f6dfe4
Ammar <ammarfarghani@gmail.com>
2023-02-16 09:39:53 +0700

    add custom sub menu in template

--- src/components/dashboard-sidebar.js
+++ src/components/dashboard-sidebar.js
@@ -3,31 +3,7 @@ import NextLink from 'next/link';
 import { useRouter } from 'next/router';
 import PropTypes from 'prop-types';
 import { Box, Divider, Drawer, Typography, useMediaQuery } from '@mui/material';
-import { ChartBar as ChartBarIcon } from '../icons/chart-bar';
-import { NavItem } from './nav-item';
-import { FileCopy, UploadFile } from '@mui/icons-material';
-import { useAuthContext } from '../contexts/auth-context';
-
-const itemList = [
-  {
-    id: 'dashboard',
-    href: '/',
-    icon: (<ChartBarIcon fontSize="small" />),
-    title: 'Dashboard'
-  },
-  {
-    id: 'request',
-    href: '/requests',
-    icon: (<UploadFile fontSize="small" />),
-    title: 'Requests'
-  },
-  {
-    id: 'document',
-    href: '/documents',
-    icon: (<FileCopy fontSize="small" />),
-    title: 'Documents'
-  },
-];
+import MenuList from './menu-list';
 
 export const DashboardSidebar = (props) => {
   const { open, onClose } = props;
@@ -36,9 +12,6 @@ export const DashboardSidebar = (props) => {
     defaultMatches: true,
     noSsr: false
   });
-
-  const { roleAccess } = useAuthContext();
-  const items = roleAccess ? itemList.filter(v => roleAccess[v.id]?.read === true) : [];
 
   useEffect(
     () => {
@@ -86,14 +59,7 @@ export const DashboardSidebar = (props) => {
           }}
         />
         <Box sx={{ flexGrow: 1 }}>
-          {items.map((item) => (
-            <NavItem
-              key={item.title}
-              icon={item.icon}
-              href={item.href}
-              title={item.title}
-            />
-          ))}
+          <MenuList />
         </Box>
         <Divider sx={{ borderColor: '#2D3748' }} />
       </Box>
